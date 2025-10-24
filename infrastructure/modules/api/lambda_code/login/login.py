6e01058250f1637a252e2f276fba03e3ed637f92
johncbolton <johncollierbolton@gmail.com>
2025-10-24 10:01:12 -0400

    Update login.py

--- infrastructure/modules/api/lambda_code/login/login.py
+++ infrastructure/modules/api/lambda_code/login/login.py
@@ -3,19 +3,20 @@ import os
 import boto3
 from botocore.exceptions import ClientError
 
-cognito_client = boto3.client('cognito-idp')
-USER_POOL_ID = os.environ.get('COGNITO_USER_POOL_ID')
-CLIENT_ID = os.environ.get('COGNITO_CLIENT_ID')
+USER_POOL_ID = os.environ.get("COGNITO_USER_POOL_ID")
+CLIENT_ID = os.environ.get("COGNITO_CLIENT_ID")
+
 
 def lambda_handler(event, context):
-    
+    cognito_client = boto3.client("cognito-idp", region_name=os.environ.get("AWS_REGION", "us-east-1"))
+
     headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "POST,OPTIONS" 
+        "Access-Control-Allow-Methods": "POST,OPTIONS"
     }
 
-    if event.get('httpMethod') == 'OPTIONS':
+    if event.get("httpMethod") == "OPTIONS":
         return {
             "statusCode": 200,
             "headers": headers,
@@ -24,32 +25,32 @@ def lambda_handler(event, context):
 
     try:
         try:
-            body = json.loads(event.get('body', '{}'))
-            username = body.get('username')
-            password = body.get('password')
-            
+            body = json.loads(event.get("body", "{}"))
+            username = body.get("username")
+            password = body.get("password")
+
             if not username or not password:
                 raise ValueError("Username and password are required.")
         except (json.JSONDecodeError, ValueError) as e:
             return {
-                "statusCode": 400, 
+                "statusCode": 400,
                 "headers": headers,
                 "body": json.dumps({"error": f"Invalid input: {str(e)}"})
             }
 
         response = cognito_client.initiate_auth(
             ClientId=CLIENT_ID,
-            AuthFlow='USER_PASSWORD_AUTH',
+            AuthFlow="USER_PASSWORD_AUTH",
             AuthParameters={
-                'USERNAME': username,
-                'PASSWORD': password
+                "USERNAME": username,
+                "PASSWORD": password
             }
         )
-        
-        auth_result = response.get('AuthenticationResult', {})
-        id_token = auth_result.get('IdToken')
-        access_token = auth_result.get('AccessToken')
-        
+
+        auth_result = response.get("AuthenticationResult", {})
+        id_token = auth_result.get("IdToken")
+        access_token = auth_result.get("AccessToken")
+
         return {
             "statusCode": 200,
             "headers": headers,
@@ -61,22 +62,22 @@ def lambda_handler(event, context):
         }
 
     except ClientError as e:
-        error_code = e.response.get('Error', {}).get('Code')
-        if error_code == 'NotAuthorizedException':
+        error_code = e.response.get("Error", {}).get("Code")
+        if error_code == "NotAuthorizedException":
             return {
-                "statusCode": 401, 
+                "statusCode": 401,
                 "headers": headers,
                 "body": json.dumps({"error": "Invalid username or password"})
             }
-        elif error_code == 'UserNotFoundException':
+        elif error_code == "UserNotFoundException":
             return {
-                "statusCode": 404, 
+                "statusCode": 404,
                 "headers": headers,
                 "body": json.dumps({"error": "User not found"})
             }
         else:
             return {
-                "statusCode": 400, 
+                "statusCode": 400,
                 "headers": headers,
                 "body": json.dumps({"error": f"Cognito error: {e.response['Error']['Message']}"})
             }
