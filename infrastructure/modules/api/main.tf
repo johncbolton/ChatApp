5fac8ee6ee27e61acacabbc45e4dd65d4d93e1b9
johncbolton <johncollierbolton@gmail.com>
2025-10-29 19:18:00 -0400

    Fix formatting in API Gateway integration dependencies

--- infrastructure/modules/api/main.tf
+++ infrastructure/modules/api/main.tf
@@ -280,7 +280,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
     aws_api_gateway_integration.login,
     aws_api_gateway_integration.signup,
-    aws_api_gateway_integration.get_upload_url
+    aws_api_gateway_integration.get_upload_url,
 
     aws_api_gateway_method.login,
     aws_api_gateway_method.signup,
